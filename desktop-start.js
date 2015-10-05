7a53f3435eebfff8fd93d9320b1c0a8853ffa57d
Alex <alexanderddking@gmail.com>
2015-10-05 18:47:31 +1000

    New Folder Structure

---
+++ desktop-start.js
@@ -0,0 +1,12 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+app.on('ready',function(){
+	var mainWindow = new BrowserWindow({
+		width:800,
+		height:600,
+		icon: ( __dirname +'res/icon.png' )
+	})
+	mainWindow.loadUrl('file://' + __dirname + '/www/index.html')
+})
+
+
