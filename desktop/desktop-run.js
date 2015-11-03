e83c03346848f75248b8f50bf3359176c4d9e033
Alex <surge@aegirs-iMac.local>
2015-11-04 08:39:52 +1000

    copy and paste

--- desktop/desktop-run.js
+++ desktop/desktop-run.js
@@ -198,12 +198,37 @@ var remote = require('remote');
 
 
 
+// var main_menu = new Menu();
+// Menu.setApplicationMenu(main_menu);
+
+
+/*====================================
+=            Default Menu            =
+====================================*/
+var Menu = require("menu");
+
+var template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 window.addEventListener('contextmenu', function (e) {
   e.preventDefault();
-  menu.popup(remote.getCurrentWindow());
+  Menu.popup(remote.getCurrentWindow());
 }, false);
-
-// var main_menu = new Menu();
-
-
-// Menu.setApplicationMenu(main_menu);
